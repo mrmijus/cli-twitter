07072f848af110e8d865ef8a01bb79734f61289f
Milos Mijuskovic <air.mijuskovic@gmail.com>
2022-10-01 22:04:21 +0200

    Added tweet logic

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import argparse
+import sys
+
+import tweepy
+
+
+def tweet(message: str) -> None:
+    twitter_auth_keys = {
+        "consumer_key": os.getenv('CONSUMER_KEY'),
+        "consumer_secret": os.getenv('CONSUMER_SECRET'),
+        "access_token": os.getenv('ACCESS_TOKEN'),
+        "access_token_secret": os.getenv('ACCESS_TOKEN_SECRET'),
+    }
+    auth = tweepy.OAuthHandler(
+        twitter_auth_keys['consumer_key'],
+        twitter_auth_keys['consumer_secret']
+    )
+    auth.set_access_token(
+        twitter_auth_keys['access_token'],
+        twitter_auth_keys['access_token_secret']
+    )
+    api = tweepy.API(auth)
+
+    try:
+        api.update_status(status=message)
+    except Exception as e:
+        raise RuntimeError(f'Could not Tweet. Error: {e}')
+
+
+def main():
+    tweet_message = sys.argv[1]
+    tweet(message=tweet_message)
+
+
+if __name__ == '__main__':
+    main()
